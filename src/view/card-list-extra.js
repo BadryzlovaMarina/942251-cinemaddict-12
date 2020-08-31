d49de2475d03bd5bc1c5bc3cf36438c6616f3a8f
Sedona <enchanted_night@mail.ru>
2020-08-31 20:57:28 +0500

    task5

---
+++ src/view/card-list-extra.js
@@ -0,0 +1,9 @@
+export const createCardListExtraTemplate = () => {
+  return (
+    `<section class="films-list--extra">
+      <h2 class="films-list__title"></h2>
+
+      <div class="films-list__container"></div>
+    </section>`
+  );
+};
